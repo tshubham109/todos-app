d1b9f769cd0b0f46e69d5afba592343f1aa1d463
tshubham109 <tshubham109@gmail.com>
2020-05-12 00:09:00 +0530

    Updates

---
+++ precache-manifest.68776dca6d24617ac617cbc09607312b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9bc3a516f192cc25405db95a9f8345",
+    "url": "/todos-app/index.html"
+  },
+  {
+    "revision": "0d7e2fdb2824d2f40a49",
+    "url": "/todos-app/static/css/main.3472f893.chunk.css"
+  },
+  {
+    "revision": "2c24b71d6132da79f974",
+    "url": "/todos-app/static/js/2.131b70c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos-app/static/js/2.131b70c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7e2fdb2824d2f40a49",
+    "url": "/todos-app/static/js/main.10cc5399.chunk.js"
+  },
+  {
+    "revision": "e4c4eeaffa1c205f538c",
+    "url": "/todos-app/static/js/runtime-main.5ca82665.js"
+  }
+]);
